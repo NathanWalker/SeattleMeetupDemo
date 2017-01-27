99ff45832caeb2ee6e80a490435092acd62bcff1
Nathan Walker <walkerrunpdx@gmail.com>
2017-01-26 22:20:44 -0800

    setup

--- platforms/ios/build/emulator/SimplePDF.framework/Headers/SimplePDF-umbrella.h
+++
@@ -1,9 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-#import "SimplePDF.h"
-
-FOUNDATION_EXPORT double SimplePDFVersionNumber;
-FOUNDATION_EXPORT const unsigned char SimplePDFVersionString[];
-
